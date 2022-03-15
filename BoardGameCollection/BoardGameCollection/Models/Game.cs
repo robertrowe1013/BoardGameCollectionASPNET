788f7eb730ef3409ab7d74c41697927feacf8002
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2022-03-14 22:07:02 -0500

    created games class and controller

---
+++ BoardGameCollection/BoardGameCollection/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace BoardGameCollection.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PlayerCount { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
