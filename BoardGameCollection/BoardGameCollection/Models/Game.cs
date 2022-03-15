e8ef41d22a48f1864c9eafef584e30aec5f78034
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2022-03-15 12:49:49 -0500

    changed player count string to min and max players ints. Wont save to db.

--- BoardGameCollection/BoardGameCollection/Models/Game.cs
+++ BoardGameCollection/BoardGameCollection/Models/Game.cs
@@ -4,11 +4,12 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string PlayerCount { get; set; }
+        public int MinPlayerCount { get; set; }
+        public int MaxPlayerCount { get; set; }
+
 
         public Game()
         {
-
         }
     }
 }
